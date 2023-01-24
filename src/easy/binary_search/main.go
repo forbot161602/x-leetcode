a651f1dabc7b9e80c01e5173141ca6c55ae33542
gorthon <gordon.lai@mlytics.com>
2023-01-25 01:28:47 +0800

    Add solution for binary search

---
+++ src/easy/binary_search/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/binary-search
+// Runtime beats 71.69%, while memory beats 30.29%.
+func main() {
+	fmt.Println(search([]int{-1, 0, 3, 5, 9, 12}, 9))
+}
---
+++ src/easy/binary_search/solution.go
@@ -0,0 +1,17 @@
+package main
+
+func search(numbers []int, target int) int {
+	lower, upper := 0, len(numbers)-1
+	for lower <= upper {
+		middle := (lower + upper) / 2
+		number := numbers[middle]
+		if number == target {
+			return middle
+		} else if number < target {
+			lower = middle + 1
+		} else {
+			upper = middle - 1
+		}
+	}
+	return -1
+}
