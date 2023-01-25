16a9694aa4aa1374b8a1e7917f200a81b74b79dd
gorthon <gordon.lai@mlytics.com>
2023-01-25 21:48:08 +0800

    Add solution for remove element

---
+++ src/easy/remove_element/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/remove-element/
+// Runtime beats 100%, while memory beats 40.27%.
+func main() {
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
+}
