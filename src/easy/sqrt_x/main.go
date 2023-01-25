ad4aaa880375a1cdac1d4d462a6307db7d3a5778
gorthon <gordon.lai@mlytics.com>
2023-01-25 23:57:28 +0800

    Add solution for sqrt x

---
+++ src/easy/sqrt_x/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/sqrtx/
+// Runtime beats 100%, while memory beats 45.95%.
+func main() {
+	fmt.Println(mySqrt(4))
+}
---
+++ src/easy/sqrt_x/solution.go
@@ -0,0 +1,17 @@
+package main
+
+func mySqrt(number int) int {
+	if number <= 1 {
+		return number
+	}
+	lower, upper := 1, number/2
+	for lower <= upper {
+		middle := (lower + upper) / 2
+		if middle*middle <= number {
+			lower = middle + 1
+		} else {
+			upper = middle - 1
+		}
+	}
+	return upper
+}
